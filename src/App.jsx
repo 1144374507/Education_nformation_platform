6ed2057db915407a63eb0b7c0494e6d8cf6d0255
1144374507 <1144374507@qq.com>
2021-01-23 15:36:03 +0800

    '第一次提交'

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
 
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 
-import Header from './components/Header/Header'
-import NavBar from './components/NavBar/NavBar'
-import Content from './components/Content/Content'
-
-
-import './app.css'
-export default class App extends Component {
-
-
-
-
+export default class App extends PureComponent {
 
   render() {
     return (
       <div className="App">
-        <Header></Header>
-        <div className='left-nav-bar'>
-          <NavBar></NavBar>
-          <Content></Content>
-        </div>
+        App......
       </div>
     );
   }
